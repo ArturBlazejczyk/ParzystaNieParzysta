22ac5eb3bda329c8f0c3eaad4edeb6da915f2aa7
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-07 18:44:26 +0100

    basic logic

--- ParzystaNieParzysta/Program.cs
+++ ParzystaNieParzysta/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Proszę podać liczbę: ");
 
+            if (!int.TryParse(Console.ReadLine(), out int number))
+                throw new Exception();
+            if(number%2 == 0)
+                Console.WriteLine("Podana liczba jest parzysta");
+            else
+                Console.WriteLine("Podana liczba jest nieparzysta");
         }
     }
 }
