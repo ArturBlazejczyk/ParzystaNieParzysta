762845dff23450abe9624009115dee3641543fd8
ArturBlazejczyk <34420486+ArturBlazejczyk@users.noreply.github.com>
2023-02-07 18:33:21 +0100

    Add project files.

---
+++ ParzystaNieParzysta/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParzystaNieParzysta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
